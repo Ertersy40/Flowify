2eb3407a39febb41f0382aa91b8cc8c263e6480b
Ertersy40 <wonk4040@gmail.com>
2024-01-18 12:50:09 +1100

    resolved playlist retrieval error

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ document.getElementById('login-button').addEventListener('click', function() {
     window.location.href = 'https://flowify-server-1ab4c9347977.herokuapp.com/login';
 });
 
+
 function fetchAndDisplayPlaylists() {
-    fetch('https://your_heroku_app_url/user-playlists')
-        .then(response => response.json())
-        .then(playlistsData => {
-            const playlistsContainer = document.getElementById('playlists-container');
-            playlistsData.items.forEach(playlist => {
-                const playlistElement = document.createElement('div');
-                playlistElement.innerHTML = `<h3>${playlist.name}</h3>`;
-                // Add more playlist details if necessary
-                playlistsContainer.appendChild(playlistElement);
-            });
-        })
-        .catch(error => console.error('Error fetching playlists:', error));
+    fetch('https://flowify-server-1ab4c9347977.herokuapp.com/user-playlists')
+    .then(response => response.json())
+    .then(playlistsData => {
+        const playlistsContainer = document.getElementById('playlists-container');
+        playlistsData.items.forEach(playlist => {
+            const playlistElement = document.createElement('div');
+            playlistElement.innerHTML = `<h3>${playlist.name}</h3>`;
+            // Add more playlist details if necessary
+            playlistsContainer.appendChild(playlistElement);
+        });
+    })
+    .catch(error => console.error('Error fetching playlists:', error));
 }
 
 document.addEventListener('DOMContentLoaded', fetchAndDisplayPlaylists);
